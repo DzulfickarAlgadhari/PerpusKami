c3d69fa85da7b896874f2f84faa16fece5f278d0
dzul <rhezaa_algadhari@yahoo.co.id>
2017-11-22 16:13:41 +0700

    1st

---
+++ app/src/main/java/com/example/dell/perpuskami/Book.java
@@ -0,0 +1,13 @@
+package com.example.dell.perpuskami;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Book extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+    }
+}
